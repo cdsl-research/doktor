f3923237c9cc3d04c43346b32f609eb3f29fa3b1
tomoyk <tmyk.kym@gmail.com>
2021-03-30 15:07:41 +0900

    healthの初期状態を定義

--- library/doktor-health/doktor_health/health.py
+++ library/doktor-health/doktor_health/health.py
@@ -3,12 +3,15 @@ from enum import Enum
 from typing import NamedTuple
 
 
-class HealthStatus(Enum):
+# 多重継承を追加
+class HealthStatus(int, Enum):
+    """ サービスの状態を簡潔に表す状態を列挙 """
     GREEN = 100
     YELLOW = 200
     RED = 300
 
 
 class Health(NamedTuple):
-    status: HealthStatus
-    description: str
+    """ サービスの健康状態を定義 """
+    status: HealthStatus = HealthStatus.GREEN
+    description: str = "It works"
