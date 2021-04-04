38d11a698cf534138a0486376b7be3fbc99af6a0
tomoyk <tmyk.kym@gmail.com>
2021-04-04 16:53:07 +0900

    support uwsgi docker

--- service_template/api_container/web/main.py
+++ service_template/api_container/web/main.py
@@ -20,4 +20,5 @@ def healthz():
         additional_msg="from Python")
 
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
