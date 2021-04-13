f86f06ef4ccfdd7118c841e09acc84f82d59f916
Takamasa Iijima <takahyon@gmail.com>
2021-04-13 22:35:52 +0900

    [add]upload page & microservice connection succeed (web<->upload)

--- service/upload/web/main.py
+++ service/upload/web/main.py
@@ -1,18 +1,60 @@
-from flask import Flask, request, jsonify
+from flask import *
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import socket
+from werkzeug.utils import secure_filename
+import asyncio
+import requests
+import os
+import uuid
+
+UPLOAD_FOLDER = 'upload_temp'
+ALLOWED_EXTENSIONS = {'pdf'}
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://mongo:27017/dev"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'hogehoge'
+
 mongo = PyMongo(app)
 db = mongo.db
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'pdf' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['pdf']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+
     hostname = socket.gethostname()
     return jsonify(
         message="Welcome to Tasks app! I am running inside {} pod!".format(hostname))
+
+
+@app.route("/web/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'pdf' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['pdf']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        id = uuid.uuid4()
+        result = {'id': id, 'file_url': filename}
+        return jsonify(data=result)
 
 
 @app.route("/tasks")
@@ -43,7 +85,7 @@ def create_task():
 def update_task(id):
     data = request.get_json(force=True)["task"]
     response = db.task.update_one({"_id": ObjectId(id)}, {
-                                  "$set": {"task": data}})
+        "$set": {"task": data}})
     if response.matched_count:
         message = "Task updated successfully!"
     else:
@@ -74,4 +116,4 @@ def delete_all_tasks():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3000, debug=True)
--- service/website/web/main.py
+++ service/website/web/main.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import *
+import os
 from doktor_health import health
+from werkzeug.utils import secure_filename
+import asyncio
+import requests
+
+UPLOAD_FOLDER = 'upload_temp'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__, static_folder="assets")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'hogehoge'
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -12,12 +26,73 @@ def show_page():
         additional_msg="from Python")
 
 
-@app.route("/generic")
-def generic():
+@app.route("/upload")
+def upload():
     return render_template(
-        'generic.html',
+        'upload.html',
         msg='Hello, ',
         additional_msg="from Python")
+
+
+def service_upload(filename):
+    # ★ポイント2
+    fileName = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    fileDataBinary = open(fileName, 'rb').read()
+    files = {'pdf': (fileName, fileDataBinary, 'application/pdf')}
+
+    # ★ポイント3
+    url = 'http://localhost:3000/web/upload'
+    response = requests.post(url, files=files)
+
+    # print(response.status_code)
+
+    print(response.content)
+    return response.json()
+
+
+@app.route("/uploading", methods=["POST"])
+def uploading():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'pdf' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['pdf']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # loop = asyncio.get_event_loop()
+            # loop.run_until_complete(asyncio.ensure_future(upload()))
+            # loop.close()
+            res = service_upload(filename)
+            return res
+            # return redirect(url_for('uploaded_file', filename=filename))
+
+    return '''
+            <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+          <input type=file name=file>
+          <input type=submit value=Upload>
+        </form>
+        '''
+
+
+# return render_template(
+#     'uploading.html',
+#     msg='Hello, ',
+#     additional_msg="from Python")
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 @app.route("/elements")
