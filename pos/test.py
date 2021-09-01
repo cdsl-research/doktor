26d1d597a9eb71f19fb3b3011b0ef48592e59cda
Takamasa Iijima <takahyon@gmail.com>
2021-09-01 16:46:53 +0900

    posのためのpod 情報取得モジュール

--- pos/test.py
+++ pos/test.py
@@ -1,24 +1,7 @@
 names = ["couchbase", "postgres", "ubuntu", "alpine", "mongo", "traefil", "redis", "busybox", "mariadb", "node"]
 
+f = open("template.yaml","r")
+tem = f.read()
 for n in names:
-    tem = """apiVersion: apps/v1
-    kind: Deployment
-    metadata:
-      name: %name
-    spec:
-      selector:
-        matchLabels:
-          app: %name
-      replicas: 1
-      template:
-        metadata:
-          labels:
-            app: %name
-        spec:
-          containers:
-          - name: flask
-            image: %name:latest
-            imagePullPolicy: IfNotPresent
-    """
     with open(f"{n}.yaml","w") as f:
         f.write(tem.replace("%name",n))
